75a3f226dc70e61ed6c8cdf271bd7d597bcc5bb4
Nathan Chon <nchon99@gmail.com>
2023-01-16 06:01:15 +0000

    Completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,10 @@
+const person = { firstName: 'Nathan', lastName: 'Chon', hobby: 'Tennis' };
+console.log(person);
+const fullName = person.firstName + ' ' + person.lastName;
+console.log("The person's name is:" + ' ' + fullName);
+person.job = 'Tennis Instructor';
+console.log("The person's cureent job is:" + ' ' + person.job);
+person.previousJob = 'Logistician';
+console.log("The person's previous job was:" + ' ' + person.previousJob);
+console.log('The complete person object: ' + person);
+console.log(person);
